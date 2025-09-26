df9283e85cec2574b370cef4edeeaf1863604d93
AhmedZia-crypto <zia14fox@gmail.com>
2025-09-26 13:44:54 +0300

    Add files via upload

---
+++ multiple expcetion.py
@@ -0,0 +1,23 @@
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    result = num1/num2
+
+    print("Result is: ", result)
+    print("Result is: ", result1)
+
+
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+
+except ValueError:
+    print("Print numerical value")
+
+except NameError as ex:
+    print("Exception is :", ex)
+
+
+except:
+    print("Some error occured")
+finally:
+    print("I will execute no matter what happens")
